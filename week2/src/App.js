8c68d94610f957da7c4992b6655a36648dbb1a11
Saksham Kushwaha <118334330+sakshamsingh1304@users.noreply.github.com>
2024-07-09 17:04:43 +0530

    Add files via upload

---
+++ week2/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const name="Satendra"
+  return (
+    <div>
+      <h1 style={{
+        color:'red',
+        textAlign: "center"
+        }}>
+        Jai Shree Ram from {name}
+      </h1>
+    </div>
+    
+  );
+}
+
+export default App;
