6158beae84e81379139ac049d72ffb10a7c55daf
Saksham Kushwaha <118334330+sakshamsingh1304@users.noreply.github.com>
2024-07-09 17:34:18 +0530

    Create ToDoScreen.js

---
+++ week2/lecture18/src/screen/ToDoScreen.js
@@ -0,0 +1,9 @@
+import { Component } from "react";
+
+export default class ToDoScreen extends Component {
+    render(){
+        return(
+            <h1 className="ui heading">To Do List</h1>
+        );
+    }
+}
