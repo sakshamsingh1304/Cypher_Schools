071cb509bce7253fa134f42476081780176fc44d
Saksham Kushwaha <118334330+sakshamsingh1304@users.noreply.github.com>
2024-07-10 15:26:55 +0530

    Create App.js

---
+++ week2/lecture19/src/App.js
@@ -0,0 +1,15 @@
+import { Component } from "react";
+import ToDoScreen from "./screen/ToDoScreen";
+
+
+class App extends Component{
+    render(){
+        return(
+            <>
+                <ToDoScreen/>
+            </>
+        );
+    }
+}
+
+export default App;
