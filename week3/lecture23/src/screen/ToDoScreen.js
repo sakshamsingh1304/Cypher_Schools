6da2954ff7a364b0704dee04cf65293b4ef9694e
Saksham Kushwaha <118334330+sakshamsingh1304@users.noreply.github.com>
2024-07-11 15:40:28 +0530

    Create ToDoScreen.js

---
+++ week3/lecture23/src/screen/ToDoScreen.js
@@ -0,0 +1,48 @@
+import {useState} from "react";
+import Task from "../component/Task";
+import AddTask from "../component/AddTask";
+
+function ToDoScreen(){
+    const [taskList, setTaskList]= useState([]);
+
+    let addTask= (task)=>{
+        setTaskList([
+            ...taskList,
+            {
+                ...task,
+                create_date: new Date()
+            }
+        ])
+    }
+    return(
+        <>
+            <div className="screen">
+                <h1 className="ui heading center">To Do List</h1>
+                <div onClick={(e)=>{
+                    setTaskList([
+                        ...taskList,
+                        {
+                            title: "Go to Mandir",
+                            description: "To worship God",
+                            create_date: new Date(),
+                        }
+                    ]);
+                    console.log(taskList);
+                }}
+                className="ui secondary button">
+                    Add Task
+                </div>
+                <section>
+                    <div className="ui cards">
+                        {taskList.map((task) => (
+                            <Task task={task}/>
+                            ))}
+                    </div>
+                </section>
+                <AddTask/> 
+            </div>
+            
+        </>
+    );
+}
+export default ToDoScreen;
