79639e1870438dc14d065ae45081a3753c07e239
Saksham Kushwaha <118334330+sakshamsingh1304@users.noreply.github.com>
2024-07-11 16:32:08 +0530

    Create TaskContext.js

---
+++ week3/lecture27/src/context/TaskContext.js
@@ -0,0 +1,43 @@
+import { Children, createContext, useState } from "react";
+import {v4 as randomUUID} from "uuid";
+
+
+const TaskContext = createContext();
+const TASK_EDITABLE_FIELD_LIST = ["title", "description"];
+
+const TaskProvider = ({children})=>{
+    const [taskList, setTaskList] = useState([]);
+
+    const addNewTask = (task)=>{
+        setTaskList([
+            ...taskList,
+            {
+                ...task,
+                create_date: new Date(),
+                taskId: randomUUID(),
+            }
+        ]);
+    };
+
+    const deleteTask = (taskId)=>{
+        setTaskList(taskList.filter((task)=> task.taskId!== taskId));
+    }
+
+    const editTask = (task)=>{
+        let tempTaskList = [...taskList];
+        for(let t of tempTaskList){
+            if(t.taskId === task.taskId){
+                TASK_EDITABLE_FIELD_LIST.forEach((field)=>(t[field]=task[field]))
+            }
+        }
+        setTaskList(taskList);
+    }
+    return(
+        <TaskContext.Provider value={{taskList, addNewTask, deleteTask, editTask}}>{children}</TaskContext.Provider>
+    )
+}
+
+
+export {TaskProvider};
+
+export default TaskContext;
