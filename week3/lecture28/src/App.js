84c28696f046ce9a37ce5abb262c9d3f0dce3ec5
Saksham Kushwaha <118334330+sakshamsingh1304@users.noreply.github.com>
2024-07-11 16:42:02 +0530

    Add files via upload

---
+++ week3/lecture28/src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import Weather from './pages/Weather';
+
+function App() {
+  return (
+    <div className="App">
+      <Weather/>
+    </div>
+    
+  );
+}
+
+export default App;
