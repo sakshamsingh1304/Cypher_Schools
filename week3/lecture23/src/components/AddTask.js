d91beceb0cf29219ab86feb83bb054db311b0564
Saksham Kushwaha <118334330+sakshamsingh1304@users.noreply.github.com>
2024-07-11 15:39:26 +0530

    Create AddTask.js

---
+++ week3/lecture23/src/components/AddTask.js
@@ -0,0 +1,112 @@
+import { useState } from "react";
+import React from 'react';
+import { Button, Form, Container } from 'semantic-ui-react';
+
+function AddTask(){
+    const [task, setTask] = useState({
+        title:"",
+        description:"",
+    });
+
+
+    let handelInputChange = (e)=>{
+        setTask({
+            ...task, 
+            [e.target.name]: e.target.value
+        });
+    }
+
+    let onFormSubmit=(e)=>{
+        e.preventDefault();
+        console.log(task);
+    }
+
+    return(
+        <>
+            <h3 className="center">Add New Task</h3>
+            <div className="ui form">
+                <form onSubmit={onFormSubmit}>
+                    <div className="field">
+                        <label>Title</label>
+                        <input type="text" 
+                            spellCheck={false}
+                            data-ms-editor={true}
+                            placeholder="Task title" 
+                            name="title"
+                            onChange={handelInputChange}
+                        />
+                    </div>
+                    <div className="field">
+                        <label>Description</label>
+                        <textarea rows="2" 
+                            spellCheck={false}
+                            data-ms-editor={true}
+                            placeholder="Description" 
+                            name="description"
+                            onChange={handelInputChange}
+                        />
+                    </div>
+                    <button type="submit" className="ui primary button">
+                        Submit
+                    </button>
+                </form>
+            </div>
+        </>
+    )
+}
+
+
+export default AddTask;
+
+
+// import React, { useState } from 'react';
+// import { Button, Form, Container } from 'semantic-ui-react';
+// import 'semantic-ui-css/semantic.min.css';
+
+// function AddTask() {
+//     const [task, setTask] = useState({
+//         title: "",
+//         description: "",
+//     });
+
+//     let handleInputChange = (e) => {
+//         setTask({
+//             ...task, 
+//             [e.target.name]: e.target.value
+//         });
+//     }
+
+//     let onFormSubmit = (e) => {
+//         e.preventDefault(); // Fix typo from `e.preventDefalut();`
+//         console.log(task);
+//     }
+
+//     return (
+//         <Container style={{ marginTop: '20px' }}>
+//             <h3 className="ui center aligned header">Add New Task</h3>
+//             <Form onSubmit={onFormSubmit}>
+//                 <Form.Field>
+//                     <label>Title</label>
+//                     <input type="text" 
+//                         placeholder="Task title" 
+//                         name="title"
+//                         onChange={handleInputChange}
+//                     />
+//                 </Form.Field>
+//                 <Form.Field>
+//                     <label>Description</label>
+//                     <textarea rows="2" 
+//                         placeholder="Description" 
+//                         name="description"
+//                         onChange={handleInputChange}
+//                     />
+//                 </Form.Field>
+//                 <Button type="submit" primary>
+//                     Submit
+//                 </Button>
+//             </Form>
+//         </Container>
+//     );
+// }
+
+// export default AddTask;
