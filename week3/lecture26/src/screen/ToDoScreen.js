b094b3bd98399952e335f394c8ad90304d0c66ab
Saksham Kushwaha <118334330+sakshamsingh1304@users.noreply.github.com>
2024-07-11 16:18:59 +0530

    Create ToDoScreen.js

---
+++ week3/lecture26/src/screen/ToDoScreen.js
@@ -0,0 +1,34 @@
+import {useContext} from "react";
+import Task from "../component/Task";
+import TaskContext from "../context/TaskContext";
+import { useNavigate } from "react-router-dom";
+
+function ToDoScreen(){
+    const {taskList}= useContext(TaskContext);
+
+    const navigate = useNavigate();
+
+    return(
+        <>
+            <div className="screen">
+                <h1 className="ui heading center">To Do List</h1>
+                <div onClick={(e)=>{
+                    navigate("add-task");
+                }}
+                className="ui secondary button">
+                    Add Task
+                </div>
+                <section>
+                    <div className="ui cards">
+                        {taskList.map((task) => (
+                            <Task task={task}/>
+                            ))}
+                    </div>
+                </section>
+                {/* <AddTask onSubmit = {addNewTask}/>  */}
+            </div>
+            
+        </>
+    );
+}
+export default ToDoScreen;
