23dfaed287034a1c04f5e30d5b3298f3d168f834
Saksham Kushwaha <118334330+sakshamsingh1304@users.noreply.github.com>
2024-07-17 19:39:33 +0530

    Delete week6/lecture58/backend/src/exceptions/middleware/librarian-middleware.js

--- week6/lecture58/backend/src/exceptions/middleware/librarian-middleware.js
+++
@@ -1,9 +0,0 @@
-const librarianMiddleware = async(req, res, next) => {
-    if(req.user.type!== 'LIBRARIAN') {
-        return res.status(403).send({message: "You are not authorized to perform this operation"});
-
-    }
-
-    next();
-}
-module.exports ={ librarianMiddleware};
